173ee32099257552ccce8772f1227d35d5b5780d
Christian Brauner <christian.brauner@ubuntu.com>
2018-06-06 11:58:52 +0200

    proxy: simplify updateProxyDevice()
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- lxd/container_lxc.go
+++ lxd/container_lxc.go
@@ -6889,37 +6889,14 @@ func (c *containerLXC) updateProxyDevice(devName string, m types.Device) error {
 		return fmt.Errorf("Can't update proxy device in stopped container")
 	}
 
-	proxyValues, err := setupProxyProcInfo(c, m)
-	if err != nil {
-		return err
-	}
-
 	devFileName := fmt.Sprintf("proxy.%s", devName)
 	pidPath := filepath.Join(c.DevicesPath(), devFileName)
-	logFileName := fmt.Sprintf("proxy.%s.log", devName)
-	logPath := filepath.Join(c.LogPath(), logFileName)
-
-	err = killProxyProc(pidPath)
+	err := killProxyProc(pidPath)
 	if err != nil {
 		return fmt.Errorf("Error occurred when removing old proxy device")
 	}
 
-	_, err = shared.RunCommand(
-		c.state.OS.ExecPath,
-		"forkproxy",
-		proxyValues.listenPid,
-		proxyValues.listenAddr,
-		proxyValues.connectPid,
-		proxyValues.connectAddr,
-		"0",
-		"0",
-		logPath,
-		pidPath)
-	if err != nil {
-		return fmt.Errorf("Error occurred when starting new proxy device")
-	}
-
-	return nil
+	return c.insertProxyDevice(devName, m)
 }
 
 func (c *containerLXC) restartProxyDevices() {
